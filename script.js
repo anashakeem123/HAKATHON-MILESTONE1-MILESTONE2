4b4885e62c23e8491db7ed71ef86c5723f37fb61
Anas Hakeem <160387360+anashakeem123@users.noreply.github.com>
2024-11-29 01:21:49 +0500

    Push Hakathon Project

--- script.js
+++ script.js
@@ -1,10 +1,41 @@
 var buttonToggle = document.getElementById('toggle');
 var get = document.getElementById('get');
+
 buttonToggle.addEventListener("click", function () {
     if (get.style.display === 'none') {
         get.style.display = 'block';
-    }
-    else {
+    } else {
         get.style.display = 'none';
+    }
+});
+
+particlesJS('particles-js', {
+    "particles": {
+        "number": {
+            "value": 80,
+            "density": { "enable": true, "value_area": 800 }
+        },
+        "color": { "value": "#ffffff" },
+        "shape": { "type": "circle", "stroke": { "width": 0, "color": "#000000" } },
+        "opacity": {
+            "value": 0.5,
+            "anim": { "enable": false }
+        },
+        "size": {
+            "value": 5,
+            "random": true,
+            "anim": { "enable": false }
+        },
+        "move": {
+            "enable": true,
+            "speed": 6,
+            "direction": "none"
+        }
+    },
+    "interactivity": {
+        "events": {
+            "onhover": { "enable": true, "mode": "repulse" },
+            "onclick": { "enable": true, "mode": "push" }
+        }
     }
 });
